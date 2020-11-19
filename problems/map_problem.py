da3741951699a5fef3ce6c86b12dfb2d9c64cdbf
Ben Lugasi <benlugasi@campus.technion.ac.il>
2020-11-19 21:59:57 +0200

    started 17

--- problems/map_problem.py
+++ problems/map_problem.py
@@ -68,7 +68,6 @@ class MapProblem(GraphProblem):
         #        `link.distance`). You don't have to specify the operator name here.
         #  Note: Generally, in order to check whether a variable is set to None you should use the expression:
         #        `my_variable_to_check is None`, and particularly do NOT use comparison (==).
-        # yield OperatorResult(successor_state=MapState(self.target_junction_id), operator_cost=7)  # TODO: remove this line!
 
     def is_goal(self, state: GraphProblemState) -> bool:
         """
